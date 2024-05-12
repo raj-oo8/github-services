edb2a9116748161d64e397b872d4f217c6e141fc
Raj <rj.sse8@outlook.com>
2024-05-12 09:37:37 +0530

    nuget updates.

--- src/Blazor.Client.HelloWorld/Program.cs
+++ src/Blazor.Client.HelloWorld/Program.cs
@@ -1,6 +1,6 @@
+using Blazor.Client.HelloWorld;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Blazor.Client.HelloWorld;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
