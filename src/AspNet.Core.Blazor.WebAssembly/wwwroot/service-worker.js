35f6d9a1193b0e6a9875110715972693a5a1b26f
Raj <rj.sse8@outlook.com>
2024-05-12 12:54:16 +0530

    added web push support

--- src/AspNet.Core.Blazor.WebAssembly/wwwroot/service-worker.js
+++ src/AspNet.Core.Blazor.WebAssembly/wwwroot/service-worker.js
@@ -2,3 +2,12 @@
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
 self.addEventListener('fetch', () => { });
+
+self.addEventListener('push', event => {
+    const message = event.data.text();
+    self.registration.showNotification("Blazor Web", {
+        body: message,
+        icon: 'icon-512.png'
+    });
+});
+
