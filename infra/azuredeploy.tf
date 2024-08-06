fab43b0003ce61b6f32cf00eb945bad2a39b2abb
Raj <rj.sse8@outlook.com>
2024-08-06 09:52:31 +0530

    refactor: Update Azure deployment workflow for provisioning and deployment steps

--- infra/azuredeploy.tf
+++ infra/azuredeploy.tf
@@ -2,21 +2,23 @@ variable "staticSites_name" {
   default = "azure-static-web-apps-001"
 }
 
-resource "azurerm_static_site" "example" {
+resource "azurerm_static_web_app" "example" {
   name                = var.staticSites_name
   location            = "East Asia"
   resource_group_name = azurerm_resource_group.example.name
   sku_tier            = "Free"
-  repository_url      = "https://github.com/raj-oo8/github-services"
-  branch              = "main"
-  staging_environment_policy  = "Enabled"
-  allow_config_file_updates   = true
-  provider                    = "GitHub"
-  enterprise_grade_cdn_status = "Disabled"
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
 
-resource "azurerm_static_site_basic_auth" "example" {
-  static_site_id               = azurerm_static_site.example.id
+resource "azurerm_static_web_app_basic_auth" "example" {
+  static_web_app_id            = azurerm_static_web_app.example.id
   name                         = "default"
   applicable_environments_mode = "SpecifiedEnvironments"
+}
+
+provider "azurerm" {
+  features {}
 }
